b55a4d318e3a1213311eab662c279888a8a4329e
thiago.ssconceicao1 <thiago.santana3@senacsp.edu.br>
2022-11-29 17:20:41 -0300

    Criando aplicação

---
+++ PrimeiroApp/FrmLogin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiroApp
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            //Close();
+            Application.Exit();
+          
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem vindo ao sistema.", "Mensagem do sistema",MessageBoxButtons.OKCancel,MessageBoxIcon.Information,MessageBoxDefaultButton.Button2);
+        }
+    }
+}
